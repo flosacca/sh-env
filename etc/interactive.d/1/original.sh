ee378763ba1f44a53d7f11b4ef49091f374cc24a
flosacca <flosacca@gmail.com>
2022-10-29 03:39:33 +0800

    Add back dircolors setup

--- etc/interactive.d/1/original.sh
+++ etc/interactive.d/1/original.sh
@@ -57,6 +57,9 @@ xterm*|rxvt*)
     ;;
 esac
 
+# enable color support of tree
+eval "$(dircolors -b)"
+
 # colored GCC warnings and errors
 #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
