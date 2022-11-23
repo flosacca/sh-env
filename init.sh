3a1177fe772936870047aff2b8b258e146833d62
flosacca <flosacca@gmail.com>
2022-11-23 16:48:59 +0800

    Prepend underscore to local functions

--- init.sh
+++ init.sh
@@ -8,52 +8,52 @@ is_zsh() {
   [ "${ZSH_VERSION-}" ]
 }
 
-puts() {
-  printf '%s\n' "$@"
-}
-
 load() {
   [ -f "$1" ] && . "$@"
 }
 
-real_dir() {
-  puts "$(dirname "$(readlink -f "$1")")"
+_puts() {
+  printf '%s\n' "$@"
 }
 
-set_vars() {
+_real_dir() {
+  _puts "$(dirname "$(readlink -f "$1")")"
+}
+
+_set_vars() {
   if is_bash; then
     shell_type=bash
-    base_dir=$(real_dir "$BASH_SOURCE")
+    base_dir=$(_real_dir "$BASH_SOURCE")
   elif is_zsh; then
     shell_type=zsh
     base_dir=${${(%):-%x}:P:h}
   fi
 }
 
-is_login() {
+_is_login() {
   case $shell_type in
     bash) shopt -q login_shell;;
     zsh) [[ -o login ]];;
   esac
 }
 
-load_dir() {
+_load_dir() {
   local p
   for p in "$base_dir/etc/$1"/*/*.sh; do
     . "$p"
   done
 }
 
-unset_all() {
+_unset_all() {
   unset shell_type base_dir
-  unset -f puts load real_dir set_vars is_login load_dir unset_all
+  unset -f _puts _real_dir _set_vars _is_login _load_dir _unset_all
 }
 
-set_vars
-if is_login; then
-  load_dir login.d
+_set_vars
+if _is_login; then
+  _load_dir login.d
 fi
 if [[ $- = *i* ]]; then
-  load_dir interactive.d
+  _load_dir interactive.d
 fi
-unset_all
+_unset_all
