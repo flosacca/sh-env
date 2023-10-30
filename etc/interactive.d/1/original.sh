196cba130150de5707b3980f1d278cf058c4efd5
flosacca <flosacca@gmail.com>
2023-10-31 05:04:51 +0800

    Improve portability

--- etc/interactive.d/1/original.sh
+++ etc/interactive.d/1/original.sh
@@ -41,7 +41,7 @@ if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
 fi
 
 # set a fancy prompt
-if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+if [ -x "$(command -v tput)" ] && tput setaf 1 >&/dev/null; then
   # We have color support; assume it's compliant with Ecma-48
   # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
   # a case would tend to support setf rather than setaf.)
